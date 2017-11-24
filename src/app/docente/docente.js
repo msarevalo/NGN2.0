43e1a9e489f7f488f6017e9a136194cc01613bc0
msarevalo <msarevalo@github.com>
2017-11-24 03:28:47 -0500

    Estilos Docente, archivo JS docente

---
+++ src/app/docente/docente.js
@@ -0,0 +1,31 @@
+function ActualizarDocente() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("actualizar").style.display="block";
+  document.getElementById("VerEstudiante").style.display="none";
+  document.getElementById("VerCursos").style.display="none";
+  document.getElementById("CargarNotas").style.display="none";
+}
+
+function VerEstudiante() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("actualizar").style.display="none";
+  document.getElementById("VerEstudiante").style.display="block";
+  document.getElementById("VerCursos").style.display="none";
+  document.getElementById("CargarNotas").style.display="none";
+}
+
+function VerCursos() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("actualizar").style.display="none";
+  document.getElementById("VerEstudiante").style.display="none";
+  document.getElementById("VerCursos").style.display="block";
+  document.getElementById("CargarNotas").style.display="none";
+}
+
+function CargarNotas() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("actualizar").style.display="none";
+  document.getElementById("VerEstudiante").style.display="none";
+  document.getElementById("VerCursos").style.display="none";
+  document.getElementById("CargarNotas").style.display="block";
+}
