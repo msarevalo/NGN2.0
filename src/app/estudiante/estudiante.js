d7a8873ae9a002feb06181b950b196d1d30231a8
msarevalo <msarevalo@github.com>
2017-11-24 02:35:41 -0500

    js de estudiante

---
+++ src/app/estudiante/estudiante.js
@@ -0,0 +1,11 @@
+function ConsultarEstudiante() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("consultar").style.display="block";
+  document.getElementById("actualizar").style.display="none";
+}
+
+function ActualizarEstudiante() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("consultar").style.display="none";
+  document.getElementById("actualizar").style.display="block";
+}
