d1bf7baa81cb1d611b8acfc3add05c164dd3bba0
msarevalo <msarevalo@github.com>
2017-11-28 05:42:14 -0500

    cambios en el administrador

---
+++ src/app/admin/admin.js
@@ -0,0 +1,116 @@
+function CrearCurso() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="block";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function ActualizarCurso() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="block";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function EliminarCurso() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="block";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function ConsultarCurso() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="block";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function CrearMareria() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="block";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function ActualizarMareria() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="block";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function EliminarMareria() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="block";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function ConsultarMareria() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="block";
+  document.getElementById("CrearDocente").style.display="none";
+}
+
+function CrearDocente() {
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("CrearCurso").style.display="none";
+  document.getElementById("ActualizarCurso").style.display="none";
+  document.getElementById("EliminarCurso").style.display="none";
+  document.getElementById("ConsultarCursos").style.display="none";
+  document.getElementById("CrearMateria").style.display="none";
+  document.getElementById("ActualizarMateria").style.display="none";
+  document.getElementById("EliminarMateria").style.display="none";
+  document.getElementById("ConsultarMateria").style.display="none";
+  document.getElementById("CrearDocente").style.display="block";
+}
